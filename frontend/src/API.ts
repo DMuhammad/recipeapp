d5d5da535bf0eb883bff8f5b6b069d1bcce84198
Dzikri Muhammad <notfnd123@gmail.com>
2023-11-16 17:36:20 +0700

    feat: handling request and response from backend

--- frontend/src/API.ts
+++ frontend/src/API.ts
@@ -1,9 +1,11 @@
+import { Recipe } from "./types";
+
 const searchRecipes = async (searchTerm: string, page: number) => {
   const baseURL = new URL("http://localhost:5000/api/recipe/search");
   baseURL.searchParams.append("searchTerm", searchTerm);
   baseURL.searchParams.append("page", page.toString());
 
-  const response = await fetch(baseURL.toString());
+  const response = await fetch(baseURL);
 
   if (!response.ok) {
     throw new Error(`HTTP Error: ${response.status}`);
@@ -12,4 +14,72 @@ const searchRecipes = async (searchTerm: string, page: number) => {
   return response.json();
 };
 
-export { searchRecipes };
+const getRecipeSummary = async (recipeId: string) => {
+  const baseURL = new URL(
+    `http://localhost:5000/api/recipe/${recipeId}/summary`
+  );
+  const response = await fetch(baseURL);
+
+  if (!response.ok) {
+    throw new Error(`HTTP Error: ${response.status}`);
+  }
+
+  return response.json();
+};
+
+const getFavouriteRecipes = async () => {
+  const baseURL = new URL("http://localhost:5000/api/recipe/favourite");
+  const response = await fetch(baseURL);
+
+  if (!response.ok) {
+    throw new Error(`HTTP Error: ${response.status}`);
+  }
+
+  return response.json();
+};
+
+const addFavouriteRecipe = async (recipe: Recipe) => {
+  const baseURL = new URL("http://localhost:5000/api/recipe/favourite");
+  const body = {
+    recipeId: recipe.id,
+  };
+
+  const response = await fetch(baseURL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+
+  if (!response.ok) {
+    throw new Error(`HTTP Error: ${response.status}`);
+  }
+};
+
+const removeFavouriteRecipe = async (recipe: Recipe) => {
+  const baseURL = new URL("http://localhost:5000/api/recipe/favourite");
+  const body = {
+    recipeId: recipe.id,
+  };
+
+  const response = await fetch(baseURL, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+
+  if (!response.ok) {
+    throw new Error(`Failed to remove favourite`);
+  }
+};
+
+export {
+  searchRecipes,
+  getRecipeSummary,
+  getFavouriteRecipes,
+  addFavouriteRecipe,
+  removeFavouriteRecipe,
+};
